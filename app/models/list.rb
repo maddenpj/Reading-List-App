9eb36ead8fe0bbc8f8f7f71ce0aee33a2c94e0c2
Your Name <patrick@patrick-VirtualBox.(none)>
2012-09-05 22:22:58 -0500

    Initial Commit

---
+++ app/models/list.rb
@@ -0,0 +1,3 @@
+class List < ActiveRecord::Base
+  attr_accessible :descrip, :image, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListApp::Application.config.secret_token = 'd349f4eb49f3e8d9761edc6f3363babab90d7f80da7e669b908b1f67a12333eada1afe39d985331dadb5ed10bcf9c143bb88f3b9b55b7ca875b512fca4a3b801'
